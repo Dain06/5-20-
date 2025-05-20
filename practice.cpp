484b32b78f270935fba7da24af0c83fe2345b86d
Dain06 <dain2554@gmail.com>
2025-05-20 11:36:36 +0900

    4-3, 4-4 추가 및 수정

--- practice.cpp
+++ practice.cpp
@@ -110,6 +110,31 @@ int main(void) {
 		}
 		printf("\n");
 	}
+    //4-3
+	int n, m;
+	printf("n = ");
+	scanf_s("%d", &n);
+	for (int i = n;i >= 1;i--) {
+		for (int j = n; j > i;j--) {
+			printf(" ");
+		}
+		for (int j = 0; j < i;j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+    //4-4
+	int n, m;
+	printf("n = ");
+	scanf_s("%d", &n);
+	for (int i = 1;i <= n;i++) {
+		for (int j = 0; j < (2 * i - 1);j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	
 	
 	return 0;
 }
